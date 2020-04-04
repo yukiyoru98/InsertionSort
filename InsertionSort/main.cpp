12fb34360fb7b832ab57cfdc927b155075814c3c
yuki lin <christinelinyunyun8@gmail.com>
2020-04-04 12:46:21 +0800

    Initial commit

--- InsertionSort/main.cpp
+++ InsertionSort/main.cpp
@@ -1,15 +1,7 @@
-//
-//  main.cpp
-//  InsertionSort
-//
-//  Created by Christine on 2020/4/4.
-//  Copyright © 2020 Christine. All rights reserved.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(){
+    
+    
     return 0;
 }
